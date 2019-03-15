a0b2e1b3e6ebc7bed2360a849b89fff16613346b
KramRul <32257436+KramRul@users.noreply.github.com>
2019-03-15 11:38:19 +0200

    Inject Config

--- BlackJack.BusinessLogic/Config/InjectConfig.cs
+++ BlackJack.BusinessLogic/Config/InjectConfig.cs
@@ -1,8 +1,7 @@
-﻿using BlackJack.BusinessLogic.Providers.Interfaces;
-using BlackJack.BusinessLogic.Services.Interfaces;
-using BlackJack.DataAccess.UnitOfWorks;
+﻿using BlackJack.DataAccess.UnitOfWorks;
 using BlackJack.DataAccess.UnitOfWorks.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace BlackJack.BusinessLogic.Config
 {
@@ -11,32 +10,6 @@ namespace BlackJack.BusinessLogic.Config
         public static void InjectConfigures(this IServiceCollection services)
         {
             services.Scan(scan => scan
-                .FromCallingAssembly()
-                    .AddClasses(classes => classes.AssignableTo<IGameService>())
-                        .AsImplementedInterfaces()
-                        .WithTransientLifetime()
-                    .AddClasses(classes => classes.AssignableTo<IPlayerService>())
-                        .AsImplementedInterfaces()
-                        .WithTransientLifetime()
-                    .AddClasses(classes => classes.AssignableTo<IAccountService>())
-                        .AsImplementedInterfaces()
-                        .WithTransientLifetime()
-                    .AddClasses(classes => classes.AssignableTo<IJwtProvider>())
-                        .AsImplementedInterfaces()
-                        .WithTransientLifetime()
-                    .AddClasses(classes => classes.AssignableTo<IHistoryService>())
-                        .AsImplementedInterfaces()
-                        .WithTransientLifetime()
-                    .AddClasses(classes => classes.AssignableTo<IRanksHelper>())
-                        .AsImplementedInterfaces()
-                        .WithTransientLifetime()
-            );
-            services.AddTransient<IUnitOfWork, DapperUnitOfWork>();
-        }
-    }
-}
-/*
- services.Scan(scan => scan
                 .FromExecutingAssembly()
                 .FromApplicationDependencies(a => a.FullName.StartsWith("BlackJack"))
                 .AddClasses(publicOnly: true)
@@ -44,4 +17,7 @@ namespace BlackJack.BusinessLogic.Config
                     filter.Where(implementation => implementation.Name.Equals($"I{service.Name}", StringComparison.OrdinalIgnoreCase)))
                 .WithTransientLifetime()
             );
-            */
+            services.AddTransient<IUnitOfWork, DapperUnitOfWork>();
+        }
+    }
+}
