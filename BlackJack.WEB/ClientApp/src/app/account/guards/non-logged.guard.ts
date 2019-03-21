f778789b17f98eaead3f51966425c32c66b07645
KramRul <32257436+KramRul@users.noreply.github.com>
2019-03-21 11:57:53 +0200

    Min refactor

--- BlackJack.WEB/ClientApp/src/app/account/guards/non-logged.guard.ts
+++ BlackJack.WEB/ClientApp/src/app/account/guards/non-logged.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivateChild, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AccountService } from 'src/app/shared/services/account.service';
 
 @Injectable({
